e5bc4f92ad65a03f84451a4c923766aaa4dff24a
vinhello <voongvincent@gmail.com>
2024-08-31 21:18:53 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import requests
 import json
+import os
 
 
 app = Flask(__name__)
@@ -24,4 +25,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
