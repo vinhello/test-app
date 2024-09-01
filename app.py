eb467c14e4267d26c82d6e31145cdaeea5b0abf2
vinhello <voongvincent@gmail.com>
2024-08-31 21:08:23 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+import requests
+import json
+
+
+app = Flask(__name__)
+
+
+def get_meme():
+    #Uncomment these two lines and comment out the other url line if you want SFW memes
+    # sr = "/wholesomememes"
+    # url = "https://meme-api.com/gimme" + sr
+    url = "https://meme-api.com/gimme"
+    response = json.loads(requests.request ("GET", url).text)
+    meme_large = response["preview"][-2]
+    subreddit = response["subreddit"]
+    return meme_large, subreddit
+
+
+@app.route('/')
+def index():
+    meme_large, subreddit = get_meme()
+    return render_template('index.html', meme_pic=meme_large, subreddit=subreddit) 
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=80)
